e61c9f43f7d670fedb36d39ba78114be08d758e0
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-26 05:12:22 +0530

    commited by n8n

--- database/01_create_categories_table.sql
+++ database/01_create_categories_table.sql
@@ -1,6 +1,8 @@
--- Epic Title: Design PostgreSQL Data Models for Products
+-- Epic Title: Design PostgreSQL Data Models for Categories
 
 CREATE TABLE categories (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL
+    name VARCHAR(100) NOT NULL,
+    description VARCHAR(255),
+    parent_id INTEGER REFERENCES categories(id)
 );
