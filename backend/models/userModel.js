86493251453a2163b6d27f98d2f6702c1fdd910c
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-26 05:21:56 +0530

    commited by n8n

---
+++ backend/models/userModel.js
@@ -0,0 +1,28 @@
+const { DataTypes, Model } = require('sequelize');
+const sequelize = require('../services/database');
+
+// Epic Title: Ensure Modular Architecture for Easy Integration
+
+class User extends Model {}
+
+User.init({
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+}, {
+  sequelize,
+  modelName: 'User',
+});
+
+module.exports = User;
