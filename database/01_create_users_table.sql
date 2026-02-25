b650f952b5c724b74bdcd8633a3e45a45e2ea87b
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-26 05:18:19 +0530

    commited by n8n

--- database/01_create_users_table.sql
+++ database/01_create_users_table.sql
@@ -1,6 +1,7 @@
--- Epic Title: Retrieve User Addresses
+-- Epic Title: Address Data Security
 
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE
+    username VARCHAR(50) NOT NULL UNIQUE,
+    is_authorized BOOLEAN DEFAULT FALSE
 );
