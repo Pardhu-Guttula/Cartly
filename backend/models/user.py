cb9591a9b660b024f7a0150afe0d9f9f72ce2cd0
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-26 05:23:59 +0530

    commited by n8n

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,14 +1,16 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Sequence
 from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
-# Epic Title: Save User Address
+# Epic Title: User Password Security
 
 Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'users'
     
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    username = Column(String(50), nullable=False, unique=True)
-    addresses = relationship("Address", back_populates="user")
+    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
+    email = Column(String(50), unique=True, nullable=False)
+    password = Column(String(255), nullable=False)
+
+    sessions = relationship("Session", back_populates="user")
