39e2e9b8fcf41fd0d06bb85acfb5ed5180bf3239
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-26 05:17:24 +0530

    commited by n8n

--- backend/models/address.py
+++ backend/models/address.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import relationship
 from cryptography.fernet import Fernet
 import os
 
-# Epic Title: Edit User Address
+# Epic Title: Delete User Address
 
 Base = declarative_base()
 key = os.environ.get('ENCRYPTION_KEY', Fernet.generate_key())
