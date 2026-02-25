aa4987d8c4424f58ade96b85da8e144785012b80
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-26 05:18:14 +0530

    commited by n8n

--- backend/models/address.py
+++ backend/models/address.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import relationship
 from cryptography.fernet import Fernet
 import os
 
-# Epic Title: Retrieve User Addresses
+# Epic Title: Address Data Security
 
 Base = declarative_base()
 key = os.environ.get('ENCRYPTION_KEY', Fernet.generate_key())
