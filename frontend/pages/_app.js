05c814100ae0aed551a2ad7ec4e82dafd2dee6a5
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-26 05:19:41 +0530

    commited by n8n

---
+++ frontend/pages/_app.js
@@ -0,0 +1,9 @@
+import '../styles/globals.css'
+
+# Epic Title: Develop Frontend Interface for Promotions
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
