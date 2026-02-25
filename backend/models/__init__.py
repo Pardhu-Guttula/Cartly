6e1f6da3676e49118e97e2bea0f437b6fa068e9c
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-26 05:17:26 +0530

    commited by n8n

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 from backend.models.user import Base as UserBase
 from backend.models.address import Base as AddressBase
 
-# Epic Title: Edit User Address
+# Epic Title: Delete User Address
 
 DATABASE_URL = 'mysql+mysqlconnector://username:password@localhost/mydatabase'
 
