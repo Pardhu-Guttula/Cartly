e6aceaf05c4b68d844f91bf89aa117e2a688d0ea
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-26 17:47:17 +0530

    commited by n8n

--- database/categories.sql
+++ database/categories.sql
@@ -1,6 +1,9 @@
-# Epic Title: Design PostgreSQL Data Models for Products
+# Epic Title: Design PostgreSQL Data Models for Categories
 
 CREATE TABLE IF NOT EXISTS categories (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(1000),
+    parent_id INTEGER,
+    FOREIGN KEY (parent_id) REFERENCES categories(id)
 );
