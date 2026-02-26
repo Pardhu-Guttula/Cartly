b65ad4784affb2998722623224afee2b0d138a22
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-26 18:58:30 +0530

    Delete shopping_cart_and_wishlist directory

--- shopping_cart_and_wishlist/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# Epic Title: Implement shopping cart and wishlist functionality
-
-from shopping_cart_and_wishlist.models.shopping_cart import ShoppingCart
-from shopping_cart_and_wishlist.models.shopping_cart_item import ShoppingCartItem
-from shopping_cart_and_wishlist.models.wishlist import Wishlist
-from shopping_cart_and_wishlist.models.wishlist_item import WishlistItem
-
-__all__ = ["ShoppingCart", "ShoppingCartItem", "Wishlist", "WishlistItem"]
