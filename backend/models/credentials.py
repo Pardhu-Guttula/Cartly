22190f798c2615d07939a6ef672c718bc238c100
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-26 17:46:13 +0530

    commited by n8n

---
+++ backend/models/credentials.py
@@ -0,0 +1,16 @@
+# Epic Title: User Signup Functionality
+
+from backend import db
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
+
+class Credentials(db.Model):
+    __tablename__ = 'credentials'
+
+    id = Column(Integer, primary_key=True)
+    password = Column(String(128), nullable=False)
+
+    user = relationship("User", uselist=False, back_populates="credentials")
+
+    def __init__(self, password: str):
+        self.password = password
