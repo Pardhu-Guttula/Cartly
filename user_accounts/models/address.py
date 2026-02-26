f4ae45c9fc7a67d404aa6a870a7d8cc16a254963
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-26 18:05:57 +0530

    commited by n8n

---
+++ user_accounts/models/address.py
@@ -0,0 +1,26 @@
+# Epic Title: Save User Address
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from backend import db
+
+class Address(db.Model):
+    __tablename__ = 'addresses'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    street = Column(String(255), nullable=False)
+    city = Column(String(100), nullable=False)
+    state = Column(String(100), nullable=False)
+    postal_code = Column(String(20), nullable=False)
+    country = Column(String(100), nullable=False)
+
+    user = relationship('User', backref=db.backref('addresses', lazy=True))
+
+    def __init__(self, user_id: int, street: str, city: str, state: str, postal_code: str, country: str):
+        self.user_id = user_id
+        self.street = street
+        self.city = city
+        self.state = state
+        self.postal_code = postal_code
+        self.country = country
