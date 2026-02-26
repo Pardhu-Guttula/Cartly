d9715b9d1149a11817b4c6ac8cb37f9954545d7c
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-26 17:46:15 +0530

    commited by n8n

---
+++ backend/__init__.py
@@ -0,0 +1,18 @@
+# Epic Title: User Signup Functionality
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://user:password@localhost/dbname'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    from backend.routes.signup import signup_bp
+    app.register_blueprint(signup_bp)
+
+    return app
