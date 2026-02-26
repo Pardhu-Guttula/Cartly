b57900304635dbc502ccebad13225214c3bd95dc
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-26 18:07:06 +0530

    commited by n8n

--- user_accounts/controllers/address_controller.py
+++ user_accounts/controllers/address_controller.py
@@ -1,12 +1,40 @@
-# Epic Title: Retrieve User Addresses
+# Epic Title: Address Data Security
 
 from flask import Blueprint, request, jsonify
 from backend import db
 from user_accounts.models.address import Address
+from functools import wraps
 
 address_bp = Blueprint('address', __name__)
 
+def authorize(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not request.headers.get("Authorization") == "Bearer your-secure-token":
+            return jsonify({"error": "Unauthorized access"}), 403
+        return f(*args, **kwargs)
+    return decorated_function
+
+@address_bp.route('/address', methods=['POST'])
+def save_address():
+    data = request.get_json()
+    user_id = data.get('user_id')
+    street = data.get('street')
+    city = data.get('city')
+    state = data.get('state')
+    postal_code = data.get('postal_code')
+    country = data.get('country')
+
+    if not all([street, city, state, postal_code, country]):
+        return jsonify({"error": "All address fields are mandatory"}), 400
+
+    address = Address(user_id=user_id, street=street, city=city, state=state, postal_code=postal_code, country=country)
+    db.session.add(address)
+    db.session.commit()
+    return jsonify({"message": "Address saved successfully"}), 201
+
 @address_bp.route('/address', methods=['GET'])
+@authorize
 def get_addresses():
     user_id = request.args.get('user_id')
 
@@ -16,11 +44,11 @@ def get_addresses():
 
     address_list = [{
         "id": address.id,
-        "street": address.street,
-        "city": address.city,
-        "state": address.state,
-        "postal_code": address.postal_code,
-        "country": address.country
+        "street": Address.decrypt_data(address.street),
+        "city": Address.decrypt_data(address.city),
+        "state": Address.decrypt_data(address.state),
+        "postal_code": Address.decrypt_data(address.postal_code),
+        "country": Address.decrypt_data(address.country)
     } for address in addresses]
 
     return jsonify(address_list), 200
