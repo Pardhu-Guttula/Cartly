313edc6b6d0d455008e897b7fd19775875cca961
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-26 16:38:10 +0530

    Delete frontend directory

--- frontend/pages/_app.js
+++
@@ -1,9 +0,0 @@
-import '../styles/globals.css'
-
-// Epic Title: Develop Visualization Front-end with Next.js
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
--- frontend/src/App.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ProductsToolbarPage from "./components/ProductsToolbarPage";
-
-export default function App() {
-  return <ProductsToolbarPage />;
-}
