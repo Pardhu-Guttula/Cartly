c6f4f7919ee3f464c5eacdbecce922df98c3bc3d
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-26 18:11:29 +0530

    commited by n8n

--- database/users.sql
+++ database/users.sql
@@ -1,8 +1,7 @@
-# Epic Title: Save User Address
+# Epic Title: User Password Security
 
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    email VARCHAR(120) NOT NULL UNIQUE,
+    email VARCHAR(120) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL
 );
