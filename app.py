a7e68596502f67f615a454b503f9d09cc550d183
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-26 17:46:51 +0530

    commited by n8n

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
-from backend import create_app
-from backend.models import init_db
-from backend.routes.auth_route import auth_bp
+# Epic Title: User Password Security
 
-# Epic Title: User Login Functionality
+from backend import create_app
 
 app = create_app()
-app.register_blueprint(auth_bp)
 
-if __name__ == "__main__":
-    init_db()
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(debug=True)
