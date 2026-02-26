6f9312c27fb6e30ee0f6512ecce9565f1bb66bfa
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-26 18:10:24 +0530

    commited by n8n

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,4 +1,4 @@
-# Epic Title: Develop Visualization Front-end with Next.js
+# Epic Title: Integrate Promotion System with Order Management
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -7,13 +7,13 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://user:password@localhost/analyticsdb'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://user:password@localhost/ordersdb'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
 
     with app.app_context():
-        from analytics_and_reporting.controllers.data_controller import data_bp
-        app.register_blueprint(data_bp, url_prefix='/api')
+        from order_management.controllers.order_controller import order_bp
+        app.register_blueprint(order_bp, url_prefix='/api')
 
     return app
