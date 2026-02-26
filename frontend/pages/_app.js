916c9e6245aabade1dc277d846e7d62a438df054
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-26 18:10:19 +0530

    commited by n8n

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,4 +1,4 @@
-# Epic Title: Establish Scalable Infrastructure using Next.js, Node.js, and PostgreSQL
+# Epic Title: Develop Visualization Front-end with Next.js
 
 import '../styles/globals.css'
 
