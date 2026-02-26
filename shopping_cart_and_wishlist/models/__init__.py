c98acacd57ac818f7dc6fb14533868decd521d5a
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-26 18:04:32 +0530

    commited by n8n

---
+++ shopping_cart_and_wishlist/models/__init__.py
@@ -0,0 +1,6 @@
+# Epic Title: Implement shopping cart and wishlist functionality
+
+from shopping_cart_and_wishlist.models.shopping_cart import ShoppingCart
+from shopping_cart_and_wishlist.models.shopping_cart_item import ShoppingCartItem
+
+__all__ = ["ShoppingCart", "ShoppingCartItem"]
