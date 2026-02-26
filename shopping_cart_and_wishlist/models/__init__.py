f5238af454ba774f8f105b1036b40b21d6590d56
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-26 18:11:12 +0530

    commited by n8n

--- shopping_cart_and_wishlist/models/__init__.py
+++ shopping_cart_and_wishlist/models/__init__.py
@@ -2,5 +2,7 @@
 
 from shopping_cart_and_wishlist.models.shopping_cart import ShoppingCart
 from shopping_cart_and_wishlist.models.shopping_cart_item import ShoppingCartItem
+from shopping_cart_and_wishlist.models.wishlist import Wishlist
+from shopping_cart_and_wishlist.models.wishlist_item import WishlistItem
 
-__all__ = ["ShoppingCart", "ShoppingCartItem"]
+__all__ = ["ShoppingCart", "ShoppingCartItem", "Wishlist", "WishlistItem"]
