2a5507953d080b035fc2517aac95ff10e05992e1
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2026-02-26 18:02:53 +0530

    commited by n8n

---
+++ user_authentication/repositories/user_repository.py
@@ -0,0 +1,13 @@
+# Epic Title: User Signup Functionality
+
+from backend import db
+from user_authentication.models.user import User
+
+class UserRepository:
+
+    def find_by_email(self, email: str):
+        return User.query.filter_by(email=email).first()
+
+    def save(self, user: User):
+        db.session.add(user)
+        db.session.commit()
